32740fad67cccb6c2466e8bfedb5280cf8ad1afd
MICHAEL COHEN (RIT Student) <mxc8518@ad.rit.edu>
2017-05-02 13:51:16 -0400

    settings work

--- client/dashboard/settings.js
+++ client/dashboard/settings.js
@@ -1,7 +1,8 @@
 
 class Setting {
-    constructor(name, display, def, change) {
+    constructor(name, type, display, def, change) {
         this.name = name;
+        this.type = type;
         Setting.map[this.name] = this;
         this.display = display;
         this.change = change;
@@ -24,19 +25,44 @@ class Setting {
     }
 }
 
+Setting.types = {
+    checkbox: 0,
+    number: 1,
+    text: 2
+};
+Object.freeze(Settings.types);
+
 Setting.map = {};
 
-Setting.config = [
-    { name: 'softTabs', display: 'Use Tabs', def: false, change: value => session.setUseSoftTabs(!value) },
-    { name: 'tabSize', display: 'Tab Size:', def: 4, change: value => session.setTabSize(value) }
-];
-
 class Menu {
-    constructor(settings) {
-        this.settings = settings;
+    constructor(name) {
+        this.name = name;
     }
 }
 
+Menu.map = {};
+Menu.map.file = new Menu('File');
+Menu.map.edit = new Menu('Edit');
+Menu.map.view = new Menu('View');
+
+Menu.list = [ Menu.map.file, Menu.map.edit, Menu.map.view ]; // display the menus in this order
+
+Setting.config = [
+    { name: 'softTabs', type: Setting.types.checkbox, display: 'Use Tabs',  def: false, change: value => session.setUseSoftTabs(!value) },
+    { name: 'tabSize',  type: Setting.types.number,   display: 'Tab Size:', def: 4,     change: value => session.setTabSize(value) }
+];
+
+Menu.map.file.contents = []; // if it's a string, it's a setting, otherwise it's a submenu or popup
+
 window.addEventListener('load', () => {
     Setting.config.forEach(val => new Setting(val.name, val.display, val.def, val.change));
+
+    Object.keys(Menu.map).forEach(key => {
+        const menu = Menu.map[key];
+        menu.contents.forEach((item, i) => {
+            if(typeof(item) === 'string')
+                menu.contents[i] = Setting.map[item];
+            else if///
+        });
+    });
 });
